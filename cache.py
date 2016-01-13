c0d294d2b29ded24b01507e87278f68239c41945
Conor Maguire <conormag94@gmail.com>
2016-01-13 16:38:52 +0000

    Add Cache class skeleton code

--- cache.py
+++ cache.py
@@ -1,5 +1,16 @@
+class Cache(object):
+
+    def __init__(self, l, k, n):
+        self.l = l
+        self.k = k
+        self.n = n
+
+    def calculate(self):
+        print(self.l, self.k, self.n)
+
 def main():
-    return
+    cache = Cache(16, 2, 4)
+    cache.calculate()
 
 if __name__ == '__main__':
     main()
